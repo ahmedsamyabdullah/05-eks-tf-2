ab8fa50a3df098da1e053aa6dbca1dbf0d48f439
ahmedsamyabdullah <engahmedsamymohamed@yahoo.com>
2025-04-12 05:34:44 +0200

    Full worker node config

--- iam-roles.tf
+++ iam-roles.tf
@@ -1,61 +1,57 @@
-# resource "aws_iam_role" "eks_role" {
-#   name = "eks_role"
-#   assume_role_policy = jsonencode({
-#     "Version" : "2012-10-17",
-#     "Statement": [{
-#       "Effect"   : "Allow",
-#       "Principal": { "Service": "eks.amazonaws.com" },
-#       "Action"   : "sts:AssumeRole"
-#     }]
-#   })
-# }
+#####################################################################################################################
+                                                # Iam Roles 
+#####################################################################################################################
 
-# resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-#   role = aws_iam_role.eks_role.name
+################## iam roles for eks cluster control plane ###################################
 
-# }
+resource "aws_iam_role" "eks_role" {
+  name = "eks_role"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement": [{
+      "Effect"   : "Allow",
+      "Principal": { "Service": "eks.amazonaws.com" },
+      "Action"   : "sts:AssumeRole"
+    }]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_service_policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-#   role = aws_iam_role.eks_role.name
-# }
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role = aws_iam_role.eks_role.name
 
-# ##### IAM Roles for Worker nodes ########
-# resource "aws_iam_role" "worker_role" {
-#   name = "worker_role"
-#   assume_role_policy = jsonencode({
-#     "Version" : "2012-10-17",
-#     "Statement": [{
-#       "Effect"   : "Allow",
-#       "Principal": { "Service": "ec2.amazonaws.com" },
-#       "Action"   : "sts:AssumeRole"
-#     }]
-#   })
-# }
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_worker_policy" {
-#   policy_arn =  "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-#   role = aws_iam_role.worker_role.name
-# }
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role = aws_iam_role.eks_role.name
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-#   role = aws_iam_role.worker_role.name
-# }
+ ##### IAM Roles for Worker nodes ########
 
-# resource "aws_iam_role_policy_attachment" "eks_container_registry" {
-#   policy_arn =  "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-#   role = aws_iam_role.worker_role.name
-# }
+resource "aws_iam_role" "worker_role" {
+  name = "worker_role"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement": [{
+      "Effect"   : "Allow",
+      "Principal": { "Service": "ec2.amazonaws.com" },
+      "Action"   : "sts:AssumeRole"
+    }]
+  })
+}
 
-# resource "aws_iam_policy_attachment" "eks_worker_vpc" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
-#   roles = aws_iam_role.worker_role.name
-# }
+resource "aws_iam_role_policy_attachment" "eks_worker_policy" {
+  policy_arn =  "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role = aws_iam_role.worker_role.name
+}
 
-# resource "aws_iam_instance_profile" "eks_worker" {
-#   name = "eks_worker_profile"
-#   role = aws_iam_role.worker_role.name
+resource "aws_iam_role_policy_attachment" "eks_worker_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role = aws_iam_role.worker_role.name
+}
 
-# }
+resource "aws_iam_role_policy_attachment" "eks_worker_container_registry" {
+  policy_arn =  "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role = aws_iam_role.worker_role.name
+}
--- worker.tf
+++ worker.tf
@@ -1,19 +1,31 @@
-# resource "aws_eks_node_group" "worker_node" {
-#   cluster_name = aws_eks_cluster.eks_cluster.name
-#   node_group_name = "samy_worker"
-#   node_role_arn = aws_iam_role.worker_role.arn
-#   subnet_ids = [ aws_subnet.public.id, aws_subnet.private.id ]
-#   ami_type       = "AL2_x86_64"
-#   instance_types = [ "t3.medium" ]
-#   ssh_key_name = "jenkins-key"
-#   remote_access {
-#     ec2_ssh_key = "jenkins-key"
-#     source_security_group_ids = [aws_security_group.worker_sg.id]
-#   }
-    
-#   scaling_config {
-#     desired_size = 2
-#     max_size     = 3
-#     min_size     = 1
-#   }
-# }
+############################################################################################################
+                                          #  EKS Worker nodes
+############################################################################################################
+resource "aws_eks_node_group" "worker_node" {
+  cluster_name = aws_eks_cluster.eks.name
+  node_group_name = "worker-node"
+  node_role_arn = aws_iam_role.worker_role.arn
+  subnet_ids = [ aws_subnet.private_1.id, aws_subnet.private_2.id]       # Note:=> in this project we will run worker node within private subnets.
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+  version = "1.32"
+  ami_type       = "AL2_x86_64"
+  instance_types = [ "t3.medium" ]
+  capacity_type = "ON_DEMAND"
+  disk_size = 20  # 20 Gib
+  force_update_version = false 
+  labels = {
+    role = "worker-node"
+  }
+
+  depends_on = [ 
+    aws_iam_role_policy_attachment.eks_worker_policy,
+    aws_iam_role_policy_attachment.eks_worker_cni_policy,
+    aws_iam_role_policy_attachment.eks_worker_container_registry
+   ] 
+
+}
